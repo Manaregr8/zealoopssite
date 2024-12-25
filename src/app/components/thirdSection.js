4bf22f3a42af9312a6f12f5ed4562d0b60cf41d0
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-12-25 07:26:00 +0000

    thirdSection error fx by using template litral

--- src/app/components/thirdSection.js
+++ src/app/components/thirdSection.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./thirdSection.module.css"; // Importing CSS module
 
 const FrameSection = () => {
+  const data3 = `In our workshop, every frame undergoes meticulous handcrafting, infused with passion and dedication. We view art as more than just a product; it's an emotion. Through our frames, we aim to convey an experience of artistry, where each piece becomes a vessel for creativity and expression. Our frames aren't just accessories; they're conduits through which art can be truly felt and appreciated. They embody perfection and serve as a testament to our commitment to excellence. Welcome to our world of meticulously crafted frames, where each piece tells a story of dedication, passion, and the transformative power of art.`;
   return (
     <section className={styles.section}>
       <div className={styles.grid}>
@@ -11,7 +12,7 @@ const FrameSection = () => {
             <span style={{ color: "#FDAF35" }}>Hand crafted</span> frames
           </h3>
           <p className={styles.p}>
-            In our workshop, every frame undergoes meticulous handcrafting, infused with passion and dedication. We view art as more than just a product; it's an emotion. Through our frames, we aim to convey an experience of artistry, where each piece becomes a vessel for creativity and expression. Our frames aren't just accessories; they're conduits through which art can be truly felt and appreciated. They embody perfection and serve as a testament to our commitment to excellence. Welcome to our world of meticulously crafted frames, where each piece tells a story of dedication, passion, and the transformative power of art.
+            {data3}
           </p>
         </div>
         <div className={styles.grid2}>
