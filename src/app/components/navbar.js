fc278190070a1a983ca7e12eea0fdf6077ae3ecb
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-12-25 07:20:20 +0000

    added navbar importing Link

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./navbar.module.css"; // Import the CSS Module
-
+import Link from "next/link";
 const Navbar = () => {
   const [menuVisible, setMenuVisible] = useState(false);
 
